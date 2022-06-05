833d05288ec4c002c68f2f42c475511fa180be5a
tianyu 12900k <ysydty@outlook.com>
2022-06-05 18:22:12 +0800

    quick dev
    branch: frontend
    user: ysydty

--- src/dty/core/Feature.ts
+++ src/dty/core/Feature.ts
@@ -10,11 +10,24 @@ export function initFeatures(): void {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const features = require("../res/features.json");
     for (const feature of Object.keys(features)) {
-        const featureState = features[feature];
-
-        Features[feature] = featureState?.enable || false;
+        Features[feature] = getToggleState(features, feature);
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).DTYFeature = Features;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getToggleState(features: any, featureName: string): boolean {
+    const featureState = features[featureName];
+    const featureDependency = featureState?.dependency;
+
+    let bDependentFeatureState = true;
+    if (featureDependency && Array.isArray(featureDependency)) {
+        for (let i = 0; bDependentFeatureState && i < featureDependency.length; ++i) {
+            bDependentFeatureState = getToggleState(features, featureDependency[i]);
+        }
+    }
+
+    return bDependentFeatureState && (featureState?.enable || false);
 }
--- src/pages/common/TYStructurePage.tsx
+++ src/pages/common/TYStructurePage.tsx
@@ -5,18 +5,23 @@ import { IShellProperty } from "src/dty/model/IShell";
 import { TYDynamicPage } from "./TYDynamicPage";
 
 export abstract class TYStructurePage extends TYDynamicPage {
+    private preHashChanged: ((ev: HashChangeEvent) => void) | null;
+
     public constructor(props: IShellProperty) {
         super(props);
+
+        this.preHashChanged = null;
     }
 
     protected override renderLoaded(): React.ReactNode {
         return <div></div>;
     }
     protected override componmentMounted(): void {
+        this.preHashChanged = window.onhashchange;
         window.onhashchange = this.onHashChanged.bind(this);
     }
     protected override componmentWillUnmounted(): void {
-        window.onhashchange = null;
+        window.onhashchange = this.preHashChanged;
     }
 
     protected abstract renderSelectBar(): React.ReactNode[];
@@ -24,6 +29,10 @@ export abstract class TYStructurePage extends TYDynamicPage {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     private onHashChanged(ev: HashChangeEvent): any {
+        if (this.preHashChanged) {
+            this.preHashChanged(ev);
+        }
+
         console.log(ev.newURL);
     }
 }
