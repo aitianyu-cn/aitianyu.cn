6054028b2c058972eaac0e70576b2c8b55229469
aitianyu macbook <aitianyu.cn@outlook.com>
2022-11-16 19:06:28 +0800

    remove unused template and add login/on controller
    branch: ts-server
    user: senyun.yao

---
+++ Tianyu-Server-User/Tianyu-Server-User/Controllers/Account/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ts_webapi_core.Model.Account;
+using ts_webapi_core.Service;
+
+namespace Tianyu.Server.User.Controllers.Account
+{
+    [Route("aitianyu/cn/user/account/login")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private static string CLIENT_COOKIE_IDENTIFY_NAME = "TIANYU_xID";
+        private static string CLIENT_COOKIE_TOKEN_NAME = "TIANYU_cID";
+
+        [HttpGet]
+        public LoginResult get()
+        {
+            HttpRequest request = Request;
+
+            string identify = string.Empty;
+            string token = string.Empty;
+
+            IRequestCookieCollection cookies = request.Cookies;
+            foreach (KeyValuePair<string, string> cookie in cookies)
+            {
+                if (cookie.Key == CLIENT_COOKIE_IDENTIFY_NAME)
+                    identify = cookie.Value;
+                else if (cookie.Key == CLIENT_COOKIE_TOKEN_NAME)
+                    token = cookie.Value;
+            }
+
+            return UserOnlineServer.Instance.Login(identify, token);
+        }
+
+        [HttpPost]
+        public LoginResult post([FromBody] LoginPost body)
+        {
+            return UserOnlineServer.Instance.Login
+                (
+                body.user, 
+                LoginHelper.ConvertUserType(body.userType), 
+                body.password, 
+                body.identify
+                );
+        }
+    }
+}
