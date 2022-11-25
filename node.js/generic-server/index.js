8e88a65e2c00e33294a5f88d5da0adc73052e5b0
tianyu 12900k <ysydty@outlook.com>
2022-11-25 20:09:14 +0800

    add language support for generic server
    branch: ts-server
    user: ysydty

--- node.js/generic-server/index.js
+++ node.js/generic-server/index.js
@@ -3,16 +3,19 @@
 const HttpHandler = require("../common/handler/HttpHandler");
 const DatabasePools = require("../common/service/DatabasePools");
 const { createServer } = require("../common/service/HttpService");
-const path = require("path");
 
 const dbConfig = require("./src/config/mysql.config.json");
 const FeatureDispatcher = require("./src/dispatcher/FeatureDispatcher");
+const LanguageDispatcher = require("./src/dispatcher/LanguageDispatcher");
 
 const databasePool = new DatabasePools(dbConfig);
 const handler = new HttpHandler();
 
 const featureDispatcher = new FeatureDispatcher(databasePool);
 featureDispatcher.createDispatches(handler);
+
+const languageDispatcher = new LanguageDispatcher(databasePool);
+languageDispatcher.createDispatches(handler);
 
 const server = createServer(
     (req, res) => {
