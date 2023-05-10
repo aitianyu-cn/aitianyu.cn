2450334747ce2d6787de1eead7bb6134e2607019
victor yao <victor.yao@sap.com>
2023-05-10 16:06:18 +0800

    new service for daily
    add daily event recording!!!!
    
    branch: ts-server
    user: victor.yao

---
+++ packages/app/daily/src/common/Types.ts
@@ -0,0 +1,24 @@
+/**@format */
+
+export interface IDailyBase {
+    author: string;
+    key: string;
+}
+
+export interface ITimerRecord extends IDailyBase {
+    time: string;
+    summary: string;
+    description: string;
+}
+
+export interface ITimerGet extends IDailyBase {
+    time: string;
+    summary: string;
+}
+
+export interface ITimerGetRecord {
+    time: string;
+    during: number;
+    summary: string;
+    description: string;
+}
