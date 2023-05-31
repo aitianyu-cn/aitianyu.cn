c038df2c0843abb95ecbd142a73712eaa74c5a3b
victor yao <victor.yao@sap.com>
2023-05-31 11:24:24 +0800

    build new release web page
    branch: ts-frontend-build
    user: victor.yao

---
+++ build/package/tianyu_shell/component/ui_bg.828ba445.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var i="tianyu_shell_ui_background";!function(){tianyuShell.core.ui&&(tianyuShell.core.ui.background={});var u=document.createElement("div");u.classList.add("ts_ui_bg"),u.id=i,document.body.appendChild(u)}()}();
