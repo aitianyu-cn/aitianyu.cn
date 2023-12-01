9a553c3577a6d301b12a311cc8b8ca7b56f99c48
victor yao <victor.yao@sap.com>
2023-12-01 15:15:14 +0800

    update index
    branch: ts-master
    user: victor.yao

--- tianyu/ui/index.ts
+++ tianyu/ui/index.ts
@@ -33,10 +33,171 @@ loadI18n().finally(() => {
 
     Promise.all([import("@aitianyu.cn/tianyu-shell/core")]).then(([core]) => {
         //
-        try {
-            core.Language.parse("123");
-        } catch (e) {
-            core.Log.error((e as unknown as any as Error).toString());
-        }
+        // try {
+        //     core.Language.parse("123");
+        // } catch (e) {
+        //     core.Log.error((e as unknown as any as Error).toString());
+        // }
+        core.Message.post(
+            core.TianyuShellUIMessageType.ERROR,
+            "1000",
+            "This is a test message for testing Message Tips. Using Error type message to test all functions in Message Tips.",
+            "Test message",
+            [
+                "first: details first string - place holder",
+                "second: details second string - place holder",
+                "third: details third string - place holder",
+            ],
+            true,
+            {
+                key: "TEST_KEY_1",
+                message: "test more message",
+                link: () => {
+                    alert("test more message");
+                },
+            },
+            {
+                key: "TEST_KEY_2",
+                message: "test trouble shot message",
+                link: () => {
+                    alert("test trouble shot message");
+                },
+            },
+        );
+
+        core.Message.post(
+            core.TianyuShellUIMessageType.WARNING,
+            "1000",
+            "This is a test message for testing Message Tips. Using Error type message to test all functions in Message Tips.",
+            "Test message",
+            [
+                "first: details first string - place holder",
+                "second: details second string - place holder",
+                "third: details third string - place holder",
+            ],
+            true,
+            {
+                key: "TEST_KEY_1",
+                message: "test more message",
+                link: () => {
+                    alert("test more message");
+                },
+            },
+            {
+                key: "TEST_KEY_2",
+                message: "test trouble shot message",
+                link: () => {
+                    alert("test trouble shot message");
+                },
+            },
+        );
+
+        core.Message.post(
+            core.TianyuShellUIMessageType.FATAL,
+            "1000",
+            "This is a test message for testing Message Tips. Using Error type message to test all functions in Message Tips.",
+            "Test message",
+            [
+                "first: details first string - place holder",
+                "second: details second string - place holder",
+                "third: details third string - place holder",
+            ],
+            true,
+            {
+                key: "TEST_KEY_1",
+                message: "test more message",
+                link: () => {
+                    alert("test more message");
+                },
+            },
+            {
+                key: "TEST_KEY_2",
+                message: "test trouble shot message",
+                link: () => {
+                    alert("test trouble shot message");
+                },
+            },
+        );
+
+        core.Message.post(
+            core.TianyuShellUIMessageType.INFO,
+            "1000",
+            "This is a test message for testing Message Tips. Using Error type message to test all functions in Message Tips.",
+            "Test message",
+            [
+                "first: details first string - place holder",
+                "second: details second string - place holder",
+                "third: details third string - place holder",
+            ],
+            true,
+            {
+                key: "TEST_KEY_1",
+                message: "test more message",
+                link: () => {
+                    alert("test more message");
+                },
+            },
+            {
+                key: "TEST_KEY_2",
+                message: "test trouble shot message",
+                link: () => {
+                    alert("test trouble shot message");
+                },
+            },
+        );
+
+        core.Message.post(
+            core.TianyuShellUIMessageType.SUCCESS,
+            "1000",
+            "This is a test message for testing Message Tips. Using Error type message to test all functions in Message Tips.",
+            "Test message",
+            [
+                "first: details first string - place holder",
+                "second: details second string - place holder",
+                "third: details third string - place holder",
+            ],
+            true,
+            {
+                key: "TEST_KEY_1",
+                message: "test more message",
+                link: () => {
+                    alert("test more message");
+                },
+            },
+            {
+                key: "TEST_KEY_2",
+                message: "test trouble shot message",
+                link: () => {
+                    alert("test trouble shot message");
+                },
+            },
+        );
+
+        core.Message.post(
+            core.TianyuShellUIMessageType.RELOAD,
+            "1000",
+            "This is a test message for testing Message Tips. Using Error type message to test all functions in Message Tips.",
+            "Test message",
+            [
+                "first: details first string - place holder",
+                "second: details second string - place holder",
+                "third: details third string - place holder",
+            ],
+            true,
+            {
+                key: "TEST_KEY_1",
+                message: "test more message",
+                link: () => {
+                    alert("test more message");
+                },
+            },
+            {
+                key: "TEST_KEY_2",
+                message: "test trouble shot message",
+                link: () => {
+                    alert("test trouble shot message");
+                },
+            },
+        );
     });
 });
